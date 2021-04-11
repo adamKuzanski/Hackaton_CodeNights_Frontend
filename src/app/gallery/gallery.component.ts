0fafad63541926b231f8ea9371f38e3dded35517
Bartek <akalik19786@gmail.com>
2021-04-11 03:53:10 +0200

    filenames

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-gallery',
@@ -7,7 +7,78 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
-  constructor() { }
+  const;
+  urls = [
+    {
+      name: 'Auta',
+      data: [
+        '/assets/Auta/1.PNG',
+        '/assets/Auta/2.PNG',
+        '/assets/Auta/3.PNG',
+        '/assets/Auta/4.PNG',
+        '/assets/Auta/5.PNG',
+        '/assets/Auta/6.PNG',
+        '/assets/Auta/7.PNG',
+      ]
+    },
+    {
+      name: 'Rower',
+      data: [
+        '/assets/Auta/1.PNG',
+        '/assets/Auta/2.PNG',
+        '/assets/Auta/3.PNG',
+        '/assets/Auta/4.PNG',
+        '/assets/Auta/5.PNG',
+      ]
+    },
+    {
+      name: 'Człowiek tyłem',
+      data: [
+        '/assets/Auta/1.PNG',
+        '/assets/Auta/2.PNG',
+        '/assets/Auta/3.PNG',
+      ]
+    },
+    {
+      name: 'Dobra maseczka',
+      data: [
+        '/assets/Auta/1.PNG',
+        '/assets/Auta/2.PNG',
+        '/assets/Auta/3.PNG',
+        '/assets/Auta/4.PNG',
+        '/assets/Auta/5.PNG',
+        '/assets/Auta/6.PNG',
+        '/assets/Auta/7.PNG',
+        '/assets/Auta/8.PNG',
+        '/assets/Auta/9.PNG',
+        '/assets/Auta/10.PNG',
+        '/assets/Auta/11.PNG',
+        '/assets/Auta/12.PNG',
+        '/assets/Auta/13.PNG',
+        '/assets/Auta/14.PNG',
+        '/assets/Auta/15.PNG',
+        '/assets/Auta/16.PNG',
+        '/assets/Auta/17.PNG',
+        '/assets/Auta/18.PNG',
+      ]
+    },
+    {
+      name: 'Zla maseczka',
+      data: [
+        '/assets/Auta/1.PNG',
+        '/assets/Auta/2.PNG',
+        '/assets/Auta/3.PNG',
+        '/assets/Auta/4.PNG',
+        '/assets/Auta/5.PNG',
+        '/assets/Auta/6.PNG',
+        '/assets/Auta/7.PNG',
+        '/assets/Auta/8.PNG',
+        '/assets/Auta/9.PNG',]
+    },
+  ];
+
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
